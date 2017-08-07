298a741d44e4e119212d358971a77baec357b474
arnon19-meet <arnon19@meet.mit.edu>
2017-08-07 11:42:22 +0300

    Stopping here

---
+++ fat_man_main.py
@@ -0,0 +1,30 @@
+
+import turtle
+import random
+
+turtle.tracer(1,0)
+
+SIZE_X=1250
+SIZE_Y=700
+
+turtle.setup(SIZE_X, SIZE_Y)
+turtle.penup()
+#lists
+pos_list=[]
+stamp_list=[]
+score_list=[]
+player_list=[]
+food_pos=[]
+
+#turtles
+food=turtle.clone()
+fat_man=turtle.clone()
+#shape
+fat_man.shape("square")
+#lists
+pos_list=[]
+stamp_list=[]
+score_list=[]
+player_list=[]
+food_pos=[]
+
